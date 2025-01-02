945b6c46d3a390169d89a593d6343a2b392de720
Marc <marcanthonycasale1@gmail.com>
2025-01-03 05:49:08 +0800

    Deploy to GitHub Pages

---
+++ build/precache-manifest.0f192d2c56310f4dc5a5f476346764d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d081aa97fb50beaa5aa0611305313e0",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "99117939d510c4072e49",
+    "url": "/My-Portfolio/static/css/main.44a1a716.chunk.css"
+  },
+  {
+    "revision": "9ea1fc1341c123bef1cd",
+    "url": "/My-Portfolio/static/js/2.75a0d43e.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/My-Portfolio/static/js/2.75a0d43e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dacce45d4e8958c74912",
+    "url": "/My-Portfolio/static/js/3.8753b1e9.chunk.js"
+  },
+  {
+    "revision": "99117939d510c4072e49",
+    "url": "/My-Portfolio/static/js/main.25e84cbe.chunk.js"
+  },
+  {
+    "revision": "ad070dc63215b2f803eb",
+    "url": "/My-Portfolio/static/js/runtime-main.4a10e8ab.js"
+  },
+  {
+    "revision": "fbe8239334ec1552f71234ca585407a9",
+    "url": "/My-Portfolio/static/media/hero-cover.fbe82393.jpg"
+  }
+]);
